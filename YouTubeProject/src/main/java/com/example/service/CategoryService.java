dd790d28204bd89fb935049772948500d156e0e5
RakhmonaliAnaqulov <rakhmonaliandaqulov@gmail.com>
2023-05-15 18:44:49 +0500

    fixed

--- YouTubeProject/src/main/java/com/example/service/CategoryService.java
+++ YouTubeProject/src/main/java/com/example/service/CategoryService.java
@@ -13,7 +13,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
