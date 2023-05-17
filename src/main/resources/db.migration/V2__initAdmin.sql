e938f6ce85e5bac9a5a831063608def4931f37f0
RuzibekKholmuminov <kholmuminovruzibek@gmail.com>
2023-05-17 13:50:56 +0500

    flyway finished

--- src/main/resources/db.migration/V2__initAdmin.sql
+++
@@ -1,2 +0,0 @@
-insert into profile(name, surname, password, photo, email, role, created_date)
-values ('TOshmat1', 'Toshatov1', '123', 'dfgdfdsf.jpg','toshmat1@mail.ru', 'ADMIN', now());
---
+++ src/main/resources/db/migration/V6__insert_admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO profile (name, surname, email, phone, password, status, role)
+VALUES ('Admin', 'Adminev', 'adminjon08@gmail.com', '+998939999999', '1122', 'ACTIVE', 'ADMIN');
