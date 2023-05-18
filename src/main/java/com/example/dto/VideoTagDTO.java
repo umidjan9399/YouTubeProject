7f1ad44780e2ce9cb50e848056ac0d3609100e2f
Mohirbek <119959961+ErkinovMoxirbek@users.noreply.github.com>
2023-05-18 17:29:36 +0500

    VideoTagDTO fixed

--- src/main/java/com/example/dto/VideoTagDTO.java
+++ src/main/java/com/example/dto/VideoTagDTO.java
@@ -1,7 +1,12 @@
 package com.example.dto;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+@Getter @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class VideoTagDTO {
 
     private Integer id;
