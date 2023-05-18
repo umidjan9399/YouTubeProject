9dabd0d8a2232671dd83d9818d8a0d7a8f6076fc
Mohirbek <119959961+ErkinovMoxirbek@users.noreply.github.com>
2023-05-18 18:26:57 +0500

    VideoTagDTO fixed

--- src/main/java/com/example/dto/VideoTagDTO.java
+++ src/main/java/com/example/dto/VideoTagDTO.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 public class VideoTagDTO {
 
     private Integer id;
-    private String video_id;
-    private Integer tag_id;
-    private LocalDateTime created_date = LocalDateTime.now();
+    private String videoId;
+    private Integer tagId;
+    private LocalDateTime createdDate;
 }
